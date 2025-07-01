16f0ca28cc77230925456caac5ce1020f6ee34c1
kipkoech <gideonkipkoech854@gmail.com>
2025-07-02 00:56:19 +0300

    this is my commit with  50%passing

--- server/models.py
+++ server/models.py
@@ -13,20 +13,56 @@ db = SQLAlchemy(metadata=metadata)
 
 class Customer(db.Model):
     __tablename__ = 'customers'
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
 
-    def __repr__(self):
-        return f'<Customer {self.id}, {self.name}>'
-
+    reviews = db.relationship(
+        'Review',
+        back_populates='customer',
+        cascade='all, delete-orphan',
+        passive_deletes=True
+    )
+    items = db.relationship(
+        'Item',
+        secondary='reviews',
+        back_populates='customers',
+        viewonly=True
+    )
 
 class Item(db.Model):
     __tablename__ = 'items'
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    price = db.Column(db.Float)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
 
-    def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
+    reviews = db.relationship(
+        'Review',
+        back_populates='item',
+        cascade='all, delete-orphan',
+        passive_deletes=True
+    )
+    customers = db.relationship(
+        'Customer',
+        secondary='reviews',
+        back_populates='items',
+        viewonly=True
+    )
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(200), nullable=False)
+
+    customer_id = db.Column(
+        db.Integer,
+        db.ForeignKey('customers.id', ondelete='CASCADE'),
+        nullable=False
+    )
+    item_id = db.Column(
+        db.Integer,
+        db.ForeignKey('items.id', ondelete='CASCADE'),
+        nullable=False
+    )
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
